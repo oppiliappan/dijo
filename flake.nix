ad5bf181a176e64c9f70a292cad870e6e8110f09
Akshay <nerdy@peppe.rs>
2021-01-27 08:55:37 +0530

    bump to v0.2.5
      - fixes installation from crates.io
      - bump cursive to v0.16

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     in rec {
       packages.my-project = naersk-lib.buildPackage {
         pname = "dijo";
-        version = "0.2.4";
+        version = "0.2.5";
         root = ./.;
       };
       defaultPackage = packages.my-project;
